f2744fec56c475d06d61a1840c075eefcd0a0cd8
WangKun <wangkun611@gmail.com>
2023-04-06 11:34:29 +0800

    Complete spdlog

--- cpp/spdlog_example1.cpp
+++ cpp/spdlog_example1.cpp
@@ -56,4 +56,18 @@ int main()
     // Registry
     auto async_file_logger_daily_ref = spdlog::get("async_file_logger_daily");
     async_file_logger_daily_ref->info("async_file_logger_daily_ref");
+
+    // Logger with multi sinks
+    {
+        auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        console_sink->set_level(spdlog::level::warn);
+        console_sink->set_pattern("[multi_sink_example] [%^%l%$] %v");
+
+        auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/multisink.txt", true);
+        file_sink->set_level(spdlog::level::trace);
+
+        spdlog::logger logger("multi_sink", {console_sink, file_sink});
+        logger.set_level(spdlog::level::debug);
+        logger.warn("multi_sink");
+    }
 }
